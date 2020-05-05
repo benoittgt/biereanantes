78d2662a5e08f3c98cda7fd287669f15bfab6145
Benoit Tigeot <benoit@hopsandfork.com>
2020-05-05 02:57:34 +0200

    quick video mode

--- src/js/script.js
+++ src/js/script.js
@@ -149,3 +149,38 @@ function addPoints(data) {
   });
 }
 
+var start = [-74.5, 40];
+var end = [74.5, 40];
+var isAtStart = true;
+
+document.getElementById('fly').addEventListener('click', function() {
+  // depending on whether we're currently at point a or b, aim for
+  // point a or b
+  var target = isAtStart ? end : start;
+
+  // and now we're at the opposite point
+  isAtStart = !isAtStart;
+
+  map.flyTo({
+    // These options control the ending camera position: centered at
+    // the target, at zoom level 9, and north up.
+    center: target,
+    zoom: 9,
+    bearing: 0,
+
+    // These options control the flight curve, making it move
+    // slowly and zoom out almost completely before starting
+    // to pan.
+    speed: 0.2, // make the flying slow
+    curve: 1, // change the speed at which it zooms out
+
+    // This can be any easing function: it takes a number between
+    // 0 and 1 and returns another number between 0 and 1.
+    easing: function(t) {
+      return t;
+    },
+
+    // this animation is considered essential with respect to prefers-reduced-motion
+    essential: true
+  });
+});
